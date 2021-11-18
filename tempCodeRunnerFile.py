cf695a5f3a04e3ae689c49eb7f887adebdc46691
zulux99 <szymon14059@gmail.com>
2021-11-18 22:51:01 +0100

    algorytm genetyczny do poprawek

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,2 @@
 
-    test = test + i.procentNaKole
-print(test)
+            else:
--- zad2.py
+++ zad2.py
@@ -5,21 +5,63 @@ class Osobnik:
         self.wartosc = wartosc
         self.procentNaKole = procentNaKole
     def __repr__(self) -> str:
-        return "Fenotyp: " + str(self.fenotyp) + " o wartości: " + str(self.wartosc)
+        return "wartosc: " + str(self.wartosc) + "  "
 def wynikFunkcji(a, b, c, d, x):
     return a * x ** 3 + b * x ** 2 + c * x + d
-def losuj(listaOsobnikow):
+def losuj(populacja):
     losowa = random.randint(0, 100)
     if losowa >= 100:
-        return listaOsobnikow[5]
-    print(losowa)
+        return populacja[5]
     suma = 0
-    for i in listaOsobnikow:
+    for i in populacja:
         suma = suma + i.procentNaKole
         if suma > losowa:
             return i
-# def krzyzowanie():
-# def mutacja():
+def funkcjaPrzystosowania(populacja):
+    wylosowane = []
+    for i in populacja:
+        wylosowane.append(losuj(populacja))
+    populacja = wylosowane
+    przelicz(populacja)
+    return populacja
+def krzyzowanie(populacja, Pk):
+    for i in range(0, int(len(populacja)), 2):
+        PkWylosowane = random.randint(0, 10) / 10
+        L = random.randint(0, 4)
+        x = list(populacja[i].fenotyp)
+        y = list(populacja[i + 1].fenotyp)
+        if (PkWylosowane < Pk):
+            x[L:], y[L:] = y[L:], x[L:]
+        x = ''.join(x)
+        y = ''.join(y)
+        populacja[i].fenotyp = x
+        populacja[i + 1].fenotyp = y
+        przelicz(populacja)
+    return populacja
+def mutacja(populacja, Pm):
+    for i in populacja:
+        PmWylosowane = random.randint(0, 10) / 10
+        L = random.randint(0, 4)
+        x = list(i.fenotyp)
+        if (PmWylosowane < Pm):
+            if x[L] == "1":
+                x[L] = "0"
+            else:
+                x[L] = "1"
+            x = ''.join(x)
+            i.fenotyp = x
+    przelicz(populacja)
+    return populacja
+def przelicz(populacja):
+    global a, b, c, d
+    for i in populacja:
+        i.wartosc = wynikFunkcji(a, b, c, d, int(i.fenotyp, 2))
+    suma = 0
+    for i in populacja:
+        suma = suma + i.wartosc
+    for i in populacja:
+        i.procentNaKole = i.wartosc / suma * 100
+    return populacja
 print("Podaj wartości funkcji ax^3 + bx^2 + cx + d")
 # a = float(input("Podaj a: "))
 # b = float(input("Podaj b: "))
@@ -27,20 +69,40 @@ print("Podaj wartości funkcji ax^3 + bx^2 + cx + d")
 # d = float(input("Podaj d: "))
 a = 0
 b = 0
-c = 0.2
-d = 5
+c = 1
+d = 1
+Pk = 0.9
+Pm = 0.2
 # Pk = input("Podaj Pk: ")
 # Pm = input("Podaj Pm: ")
-listaOsobnikow = []
+populacja = []
 for i in range(0, 6):
     random5bitBinaryNumber = format(random.randint(0, 31), "05b")
     wartoscY = round(wynikFunkcji(a, b, c, d, int(random5bitBinaryNumber, 2)), 2)
-    listaOsobnikow.append(Osobnik(random5bitBinaryNumber, wartoscY))
-suma = 0
-for i in listaOsobnikow:
-    suma = suma + i.wartosc
-test = 0
-for i in listaOsobnikow:
-    i.procentNaKole = i.wartosc / suma * 100
-    test = test + i.procentNaKole
-print(losuj(listaOsobnikow))
+    populacja.append(Osobnik(random5bitBinaryNumber, wartoscY))
+maksymalnaSuma = 0
+licznik = 0
+licznikIteracji = 0
+while licznik < 10:
+    przelicz(populacja)
+    # print("PIERWOTNA")
+    # print(populacja)
+    populacja = funkcjaPrzystosowania(populacja)
+    # print("PO PRZYSTOSOWANIU")
+    # print(populacja)
+    krzyzowanie(populacja, Pk)
+    # print("PO KRZYZOWANIU")
+    # print(populacja)
+    mutacja(populacja, Pm)
+    # print("PO MUTACJI")
+    print(populacja)
+    maksAktualny = 1
+    licznikIteracji += 1
+    for i in populacja:
+        maksAktualny += i.wartosc
+    if maksymalnaSuma == maksAktualny:
+        licznik += 1
+    if maksymalnaSuma < maksAktualny:
+        maksymalnaSuma = maksAktualny
+print("MAKS:", maksymalnaSuma)
+print("Licznik:", licznikIteracji)
